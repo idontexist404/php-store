44ec6e6b29425527ecc0694e0872a38484e24494
Highlander Magno <highlander9200@gmail.com>
2022-02-08 08:02:21 -0300

    defining constants for database connections

--- config.php
+++ config.php
@@ -2,3 +2,10 @@
 
 define('APPNAME',           'PHPSTORE');
 define('APPVERSION',        '1.0.0');
+
+//MYSQL
+define('MYSQL_SERVER',      'localhost');
+define('MYSQL_DATABASE',    'php_store');
+define('MYSQL_USER',        'user_php_store');
+define('MYSQL_PASS',        '');
+define('MYSQL_CHARSET',     'utf8');
