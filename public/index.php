211a27cce57bffe2a4db30bb32ffaf682ad42600
Highlander Magno <highlander9200@gmail.com>
2022-02-11 17:08:40 -0300

    testing git stuff

--- public/index.php
+++ public/index.php
@@ -13,14 +13,9 @@ require_once('../vendor/autoload.php');
 
 $db = new Database();
 $customers = $db->select("SELECT * FROM customers");
-<<<<<<< HEAD
 echo '<pre>';
 print_r($customers);
 echo '</pre>';
-=======
-$db->insert("INSERT TEST");
 
-print_r($customers);
->>>>>>> 095c86d (whoops, forgot to delete!)
-
+//hello
 // phpinfo();
